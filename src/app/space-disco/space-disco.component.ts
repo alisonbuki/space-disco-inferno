653868874c9443ff04d6fd77cb8873b52c5c923b
Alison Buki <alison.buki@owensoftware.com>
2016-12-14 21:35:57 -0500

    add threejs, make first component

--- src/app/space-disco/space-disco.component.ts
+++ src/app/space-disco/space-disco.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-space-disco',
   templateUrl: './space-disco.component.html',
-  styleUrls: ['./space-disco.component.css']
+  styleUrls: ['space-disco.component.scss']
 })
 export class SpaceDiscoComponent implements OnInit {
 
