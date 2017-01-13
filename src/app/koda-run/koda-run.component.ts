ed17c752eb5c9c0d335451c0787a9d715490294c
Alison Buki <alison.buki@owensoftware.com>
2017-01-13 00:39:41 -0500

    add phaser/threejs, break some stuff

--- src/app/koda-run/koda-run.component.ts
+++ src/app/koda-run/koda-run.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+//import * as phaser from 'phaser';
+
+//import * as three from 'three';
+
+//declare var phaser: any;
 
 @Component({
   selector: 'app-koda-run',
@@ -6,10 +11,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./koda-run.component.scss']
 })
 export class KodaRunComponent implements OnInit {
+  //game: any;
 
-  constructor() { }
+  constructor() {
+   // this.game = new phaser.game();
+  }
 
   ngOnInit() {
+    //var game = new phaser.game();
+  // console.log(game);
   }
 
 }
